1ad1382c537c863d78ff8809fb1cab6ad18b18c6
CryptoKiddd <avaliani1232@gmail.com>
2023-07-15 16:30:48 +0400

    bug fix:store name update,

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -1,6 +1,8 @@
-import prismadb from "@/lib/prismadb";
-import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
 
 export async function PATCH(
   req: Request,
@@ -9,63 +11,66 @@ export async function PATCH(
   try {
     const { userId } = auth();
     const body = await req.json();
+
     const { name } = body;
 
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 });
+    if(!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
     }
-    if (!name) {
+
+    if(!name) {
       return new NextResponse("Name is required", { status: 400 });
     }
 
-    if (!params.storeId) {
-      return new NextResponse("Not correct store id", { status: 404 });
+    if(!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
     }
 
     const store = await prismadb.store.updateMany({
-      where:{
+      where: {
         id: params.storeId,
         userId,
       },
       data: {
-        name: name,
-      },
+        name
+      }
     });
-    console.log('updated store')
-
+  
     return NextResponse.json(store);
 
+    
   } catch (error) {
-    console.log("[store_patch]", error);
-    return new NextResponse("internal error", { status: 500 });
+    console.log('[STORE_PATCH]', error);
+    return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
+
 
 export async function DELETE(
-  _req: Request,
+  req: Request,
   { params }: { params: { storeId: string } }
 ) {
   try {
     const { userId } = auth();
 
     if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 });
+      return new NextResponse("Unauthenticated", { status: 403 });
     }
 
     if (!params.storeId) {
-      return new NextResponse("Not correct store id", { status: 404 });
+      return new NextResponse("Store id is required", { status: 400 });
     }
 
     const store = await prismadb.store.deleteMany({
       where: {
         id: params.storeId,
-        userId,
-      },
+        userId
+      }
     });
-
+  
     return NextResponse.json(store);
   } catch (error) {
-    console.log("[store_patch]", error);
-    return new NextResponse("internal error", { status: 500 });
+    console.log('[STORE_DELETE]', error);
+    return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
--- components/SettingsForm.tsx
+++ components/SettingsForm.tsx
@@ -10,7 +10,14 @@ import { Separator } from "./ui/separator";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 import { Input } from "./ui/input";
 import { toast } from "react-hot-toast";
 import axios from "axios";
@@ -26,9 +33,8 @@ const formSchema = Z.object({
 type SettingsFormValues = Z.infer<typeof formSchema>;
 
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
-
   const params = useParams();
-  const router = useRouter()
+  const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
 
@@ -37,22 +43,19 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     defaultValues: initialData,
   });
 
-
-  const onSubmit = async(data:SettingsFormValues)=>{
-    console.log(data)
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
     try {
       setLoading(true);
-      await axios.patch(`/api/stores/${params.storeId}`,data);
-      router.refresh()
-      toast.success('Store Updated')
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success("Store Updated");
     } catch (error) {
-
-      toast.error("Something went wrong")
-      
-    }finally{
-      setLoading(false)
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <>
@@ -64,30 +67,40 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     "
       >
         <Heading title="Settings" desc="Manage Store preferences" />
-        <Button disabled={loading} variant="destructive" size="icon" onClick={() =>setOpen(true)}>
+        <Button
+          disabled={loading}
+          variant="destructive"
+          size="icon"
+          onClick={() => setOpen(true)}
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
       <Separator />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 w-full'>
-            <div className="grid grid-cols-3 gap-8">
-                <FormField control={form.control} name='name' render={({field})=>(
-                    <FormItem>
-                        <FormLabel>Name</FormLabel>
-                        <FormControl>
-                            <Input disabled={loading} placeholder={field.value} />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )} />
-            </div>
-            <Button disabled={loading} type='submit'>
-                Save Changes
-            </Button>
-
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => {return(
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input disabled={loading} placeholder={field.value} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}}
+            />
+          </div>
+          <Button disabled={loading} type="submit">
+            Save Changes
+          </Button>
         </form>
-
       </Form>
     </>
   );
